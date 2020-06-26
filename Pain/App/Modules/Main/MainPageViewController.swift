0675358704df79286d5cdeb7bcc87a36414a3295
leoramoxx <leandroaaramos@gmail.com>
2020-06-26 14:03:38 -0300

    scrollview programmatically

--- Pain/App/Modules/Main/MainPageViewController.swift
+++ Pain/App/Modules/Main/MainPageViewController.swift
@@ -41,6 +41,23 @@ class MainPageViewController: UIViewController {
         return label
     }()
     
+    lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        return scroll
+    }()
+    
+    lazy var contentview: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    lazy var topLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Topo"
+        label.textColor = UIColor.black
+        return label
+    }()
+    
     var presenter: ViewToMainPagePresenterProtocol?
 
     override func viewDidLoad() {
@@ -65,6 +82,9 @@ extension MainPageViewController: ViewLayoutProtocol {
         navigationView.addSubview(profileView)
         navigationView.addSubview(usernameLabel)
         navigationView.addSubview(emailLabel)
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentview)
+        scrollView.addSubview(topLabel)
     }
     
     func setupConstranits() {
@@ -91,6 +111,29 @@ extension MainPageViewController: ViewLayoutProtocol {
             maker.top.equalTo(usernameLabel.snp.bottom).offset(20)
             maker.leading.equalTo(profileView.snp.trailing).offset(10)
             maker.width.equalTo(250)
+            maker.height.equalTo(20)
+        }
+        
+        scrollView.snp.makeConstraints { (maker) in
+            maker.top.equalTo(navigationView.snp.bottom).offset(0)
+            maker.width.equalToSuperview()
+            maker.bottom.equalToSuperview()
+        }
+        
+        contentview.snp.makeConstraints { (maker) in
+            maker.top.bottom.equalTo(self.scrollView)
+            maker.left.right.equalTo(self.view)
+            maker.height.equalTo(1500)
+            maker.width.equalTo(self.scrollView)
+            
+            // make.centerX.equalTo(self.scrollView)
+            // make.centerY.equalTo(self.scrollView)
+        }
+        
+        topLabel.snp.makeConstraints { (maker) in
+            maker.top.equalTo(contentview.snp.top).offset(10)
+            maker.centerX.equalTo(contentview)
+            maker.width.equalTo(100)
             maker.height.equalTo(20)
         }
     }
